4796aacff82568705ead8320bc8b963bc39a4397
chunmengliu666 <chunmengliu666@gmail.com>
2025-06-24 17:03:09 +0800

    Add files via upload

---
+++ code_ms_coco_2014/configs/config_pascal_setting.py
@@ -0,0 +1,31 @@
+from box import Box
+from configs.base_config import base_config
+
+
+config = {
+    "gpu_ids": "1",
+    "box_threshold": 0.3,
+    "text_threshold": 0.25,
+    "batch_size": 1,
+    "val_batchsize": 1,
+    "num_workers": 4,
+    "num_epochs": 50,
+    "max_nums": 50,
+    "num_points": 5,
+    "resume": False,
+    "dataset": "PascalVOC",
+    "visual": False,
+    "load_type": "soft",
+    "prompt": "box",
+    "out_dir": "output/PascalVOC_box/wesam/",
+    "name": "base",
+    "corrupt": None,
+    "visual": False,
+    "opt": {
+        "learning_rate": 1e-4,
+    }
+}
+
+cfg = Box(base_config)
+cfg.merge_update(config)
+
